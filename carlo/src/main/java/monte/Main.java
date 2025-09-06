6a09d46c18921f008148f732a00f7d70e90d3e5f
xu jiejie <3082288329@qq.com>
2025-09-06 10:22:17 +0800

    new

---
+++ carlo/src/main/java/monte/Main.java
@@ -0,0 +1,7 @@
+package monte;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- learning/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module xujiejie {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens xujiejie.controller to javafx.fxml;
-    exports xujiejie;
-}
