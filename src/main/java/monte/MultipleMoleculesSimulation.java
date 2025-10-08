6dd2f994f57279da8a2ccc2589611b7464bce271
xu jiejie <3082288329@qq.com>
2025-10-08 11:11:50 +0800

    change over

--- src/main/java/monte/MultipleMoleculesSimulation.java
+++ src/main/java/monte/MultipleMoleculesSimulation.java
@@ -8,6 +8,8 @@ public class MultipleMoleculesSimulation {
     
     private List<Molecule> molecules;
     private double simulationTime;
+
+    private List<Integer> photonCounts;
     
     public MultipleMoleculesSimulation(int numMolecules, double simulationTime) {
         this.molecules = new ArrayList<>();
@@ -44,47 +46,48 @@ public class MultipleMoleculesSimulation {
         System.out.println("模拟完成，耗时: " + (endTime - startTime) + " ms");
     }
 
-    public List<Integer> getPhotonCounts(double timeStep) {
-        // 收集所有分子的发光时间
+    
+    public void getPhotonCounts(double simulationTime , int step) {
+
         List<Double> allPhotonTimes = new ArrayList<>();
         for (Molecule molecule : molecules) {
             allPhotonTimes.addAll(molecule.getPhotonTimes());
         }
         
         Collections.sort(allPhotonTimes);
-        
-        // 计算时间范围
-        double minTime = allPhotonTimes.get(0);
-        double maxTime = allPhotonTimes.get(allPhotonTimes.size() - 1);
-        
-        // 计算需要的时间段数量
-        int numBins = (int) Math.ceil((maxTime - minTime) / timeStep) + 1;
-        
+
         // 初始化光子计数数组
-        List<Integer> photonCounts = new ArrayList<>(numBins);
-        for (int i = 0; i < numBins; i++) {
+        this.photonCounts = new ArrayList<>();
+        // Exception in thread "main" java.lang.OutOfMemoryError: Requested array size exceeds VM limit
+        /* ok 啊,除以 10 就解决了 */
+
+        // 初始化
+        for (int i = 0; i < simulationTime / step; i++) {
             photonCounts.add(0);
         }
         
-        // 统计每个时间段的光子数
-        for (double photonTime : allPhotonTimes) {
-            int binIndex = (int) ((photonTime - minTime) / timeStep);
-            if (binIndex < numBins) {
-                photonCounts.set(binIndex, photonCounts.get(binIndex) + 1);
-            }
+        // 统计每10纳秒内的光子数量
+        for (double time : allPhotonTimes) {
+            int index = (int)(time / step);
+            photonCounts.set(index, photonCounts.get(index) + 1);
         }
-        
-        return photonCounts;
+
     }
     
     public static void main(String[] args) {
         /* 多个分子 */
-        int numMolecules = 100;
-        double simulationTime = 300 * Math.pow(10, 9)/* 秒 */;
+        int numMolecules = 10;
+        double simulationTime = 3 * Math.pow(10, 9)/* 秒 */;
         
         MultipleMoleculesSimulation simulation = new MultipleMoleculesSimulation(numMolecules, simulationTime);
         
         simulation.simulateParallel();
         
+        simulation.getPhotonCounts(simulationTime, 10000);  // 每1毫秒一个时间段
+
+        for (int i = 0; i < simulation.photonCounts.size(); i++) {
+            System.out.println("时间 " + (i) + " ms - " + (i + 1 ) + " ms: " + simulation.photonCounts.get(i) + " 个光子");
+        }
+
     }
 }
